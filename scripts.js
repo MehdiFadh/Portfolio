2cc7b7b470927f7aaac5cc490ada5f5c683bb788
MehdiFadh <mfadhloune@gmail.com>
2024-07-22 01:29:31 +0100

    test

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// Ajoutez des animations ou des interactions JavaScript ici si nécessaire
+document.addEventListener('DOMContentLoaded', function() {
+    const heroContent = document.querySelector('.hero-content');
+    heroContent.classList.add('fade-in');
+});
